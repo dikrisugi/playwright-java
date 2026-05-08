7c3ebdebca28854d395e2a87f2648acfcb57c607
Dikri Sugiaman <dikrisugiaman@gmail.com>
2026-05-08 10:24:51 +0700

    fix headless for CI

--- src/test/java/tests/TestBooking.java
+++ src/test/java/tests/TestBooking.java
@@ -29,7 +29,7 @@ public class TestBooking {
   public void setup() {
     playwright = Playwright.create();
     browser = playwright.chromium().launch(
-      new BrowserType.LaunchOptions().setHeadless(false)
+      new BrowserType.LaunchOptions().setHeadless(true)
     );  
   }
   
